5e4a5a8b63a31d0768bba9791194fda82b8da9a1
Matt Groberg <grobergmg@gmail.com>
2019-10-18 14:27:54 -0700

    test commit

--- dice-roller.py
+++ dice-roller.py
@@ -1,5 +1,8 @@
-def main():
-  print('You rolled a cool die')
-
-if __name__== "__main__":
-  main()
+import random
+dice_rolls = 2
+dice_sum = 0
+for i in range(0,dice_rolls):
+  roll = random.randint(1,6)
+  dice_sum += roll
+  print(f'You rolled a {roll}')
+print('You have rolled a total of {dice_sum}')
